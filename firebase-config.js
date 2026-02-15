5f62693c11e5b074a00fe4d9f8f293037b957534
Wulala <wulala@WulaladeMacBook-Air.local>
2026-02-15 23:13:28 +0800

    Remove leaked Firebase key and bump app version

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,3 @@
-window.SUDOKU_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAdHvysgp6GQK4tCwOvx6it5CA_bjo01zk",
-  authDomain: "sudokuzen-f2aa3.firebaseapp.com",
-  projectId: "sudokuzen-f2aa3",
-  storageBucket: "sudokuzen-f2aa3.firebasestorage.app",
-  messagingSenderId: "123072021375",
-  appId: "1:123072021375:web:a7cee11c8bd7f6dffd904a"
-};
+// Do not commit real Firebase keys to git.
+// Fill this object locally when you need leaderboard integration.
+window.SUDOKU_FIREBASE_CONFIG = null;
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2026.02.15.1';
+const CACHE_VERSION = '2026.02.15.2';
 const CACHE_NAME = `sudoku-zen-${CACHE_VERSION}`;
 const ASSETS = [
     './',
